5b25cb4d69d1efee44e5dc197b80a81eb3e57330
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-14 16:35:06 -0400

    added more methods to the class

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ class Student {
   constructor(firstName, lastName, year) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.year = year;
+    this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+
+  markLate() {
+    this.tardies++;
+    return `Your full name is ${this.firstName} ${this.lastName} has been late ${this.tardies}`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
   }
 }
+
+let peter = new Student('Peter', 'MoDavis');
+let kitty = new Student('Kitty', 'Kat', 1);
+
+console.log(kitty.fullName());
+console.log(kitty.markLate());
+console.log(kitty.markLate());
+console.log(kitty.addScore(5));
