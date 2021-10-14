43a8b6ee9de811ba0c74181b8df0ba812af180c5
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-14 17:39:34 -0400

    added linked list notes

--- app.js
+++ app.js
@@ -44,3 +44,21 @@ let kitty = new Student('Kitty', 'Kat', 1);
 // console.log(kitty.addScore(8));
 // console.log(Student.EnrollStudents());
 // console.log(kitty.calculateAverage());
+
+//SINGLY LINKED LIST
+//A data structure that contains a head, tail and length property.
+//A list consists of nodes, and each node has a value and a pointer to another node or null.
+//EACH NODE IS ONLY ONE DIRECTIONALLY TO THE NEXT NODE
+
+//COMPARISONS WITH ARRAYS
+//*************************************************** */
+//LISTS
+//Do not have indexes!
+//Connected via nodes with a next pointer
+//Random access is not allowed
+//******************************************* */
+//ARRAYS
+//Indexed in order!
+//Insertion and deletion can be expensive
+//Can quickly be accessed at a specific index
+//******************************************************** */
