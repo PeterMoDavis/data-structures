20d6fbf24ac6b9531d58b888b6cc22159b8622a5
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-14 16:12:23 -0400

    added initial html file for console

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //Data Structures
+
+//Classes
+class Student {
+  constructor(firstName, lastName, year) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.year = year;
+  }
+}
