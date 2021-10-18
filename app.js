2479d7cfd436c5ed6ec3e11e26f22a1cfbc95d88
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-18 18:43:49 -0400

    added pop and shift to our linked list class.

--- app.js
+++ app.js
@@ -90,6 +90,36 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
 }
 
 // var first = new Node('Hi');
@@ -103,5 +133,4 @@ list.push('HELLO');
 list.push('there');
 list.push('Bobby');
 
-console.log(list.head.next.next);
 // list.push('GOODBYE');
