1ba9e353391373b4b5ed95c2440568fcb44716e8
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-18 11:17:55 -0400

    added initial singly linked list class

--- app.js
+++ app.js
@@ -62,3 +62,31 @@ let kitty = new Student('Kitty', 'Kat', 1);
 //Insertion and deletion can be expensive
 //Can quickly be accessed at a specific index
 //******************************************************** */
+// piece of data  - val
+//reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {}
+}
+
+// var first = new Node('Hi');
+// first.next = new Node('there');
+// first.next.next = new Node('how');
+// first.next.next.next = new Node('are');
+// first.next.next.next.next = new Node('you');
+
+let list = new SinglyLinkedList();
+list.push('HELLO');
+list.push('GOODBYE');
