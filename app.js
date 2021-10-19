7e79aae928a6e1edb743d4eb6017451c2b6a3cf1
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-19 18:30:52 -0400

    added pop method to the doubly linked list class

--- app.js
+++ app.js
@@ -272,6 +272,21 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+    return currentTail;
+  }
 }
 
 let list = new DoublyLinkedList();
