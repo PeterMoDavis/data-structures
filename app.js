755851770ec3338f4d0998efd4bf10b74f35369e
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-19 18:14:17 -0400

    added the push method to the double linked list class

--- app.js
+++ app.js
@@ -259,4 +259,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+
+list.push('FIRST');
+list.push('SECOND');
+list.push('THIRD');
+list.push('FOURTH');
