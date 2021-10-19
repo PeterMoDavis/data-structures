002f243141d1080f230841fe4da96fb7e86ceeca
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-10-19 17:05:41 -0400

    finished the singly linked list class with recap and follow up notes

--- app.js
+++ app.js
@@ -223,3 +223,15 @@ list.push('THIRD');
 list.push('FOURTH');
 
 // list.push('GOODBYE');
+
+//BIG O of Singly Linked Lists
+
+// Insertion- O(1)
+// Removal - It depends... REmoving from the front O(1) or Removing from the end O(N);
+// Searching - O(N)
+// Acess- O(N)
+
+//RECAP
+//Singly Linked Lists are an excellent alternative to arrays when insertion and deletion at the beginning are frequently required.
+//Arrays contan a built in index whereas Linked Lists do not.
+//The idea of a list data structure that consists of nodes is the foundation for other data structures like Stacks and Queues.
