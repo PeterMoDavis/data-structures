f3965ef4143b00a98227394ce38fd80adec9b17a
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-11-02 17:10:24 -0400

    added removeEdge and removeVertex methods to the graph class

--- app.js
+++ app.js
@@ -1202,9 +1202,34 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
 
-let bigDog = new Graph();
-bigDog.addVertex('yuki');
-bigDog.addVertex('bobby');
-console.log(bigDog.adjacencyList);
+let g = new Graph();
+g.addVertex('Dallas');
+g.addVertex('Tokyo');
+g.addVertex('Aspen');
+g.addVertex('Los Angeles');
+g.addVertex('Hong Kong');
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Dallas', 'Aspen');
+g.addEdge('Hong Kong', 'Tokyo');
+g.addEdge('Hong Kong', 'Dallas');
+g.addEdge('Los Angeles', 'Hong Kong');
+g.addEdge('Los Angeles', 'Aspen');
+
+console.log(g.adjacencyList);
