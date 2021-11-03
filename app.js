176398ba95994c6614759b9ed99f31bdf064b58e
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-11-03 14:40:29 -0400

    added depth first recursive traversal to the graph class

--- app.js
+++ app.js
@@ -1189,6 +1189,27 @@ Most data in the real-world tends to lend itself to sparser and/or larger graphs
 
 
 WE ARE BUILDING AN UNDIRECTED GRAPH
+
+GRAPH TRAVERSAL
+
+Visiting/Updating/Checking
+each vertex in a graph
+
+
+GRAPH TRAVERSAL USES
+
+-Peer to peer networking
+-Web crawlers
+-Finding "closest"
+matches/recommendations
+-Shortest path problems
+    -GPS Navigation
+    -Solving mazes
+    -AI (shortest path to win the game)
+
+    DEPTH FIRST TRAVERSAL
+
+    There is no root node as there is in a tree.  
 */
 
 class Graph {
@@ -1217,19 +1238,46 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex];
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+    return result;
+  }
 }
 
 let g = new Graph();
-g.addVertex('Dallas');
-g.addVertex('Tokyo');
-g.addVertex('Aspen');
-g.addVertex('Los Angeles');
-g.addVertex('Hong Kong');
-g.addEdge('Dallas', 'Tokyo');
-g.addEdge('Dallas', 'Aspen');
-g.addEdge('Hong Kong', 'Tokyo');
-g.addEdge('Hong Kong', 'Dallas');
-g.addEdge('Los Angeles', 'Hong Kong');
-g.addEdge('Los Angeles', 'Aspen');
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
 
-console.log(g.adjacencyList);
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+console.log(g.depthFirstRecursive('A'));
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
