17cfc1f46a810f3c7468342788b08a75798c63d2
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-11-05 17:42:20 -0400

    added helper methods to radix sort

--- app.js
+++ app.js
@@ -1568,5 +1568,30 @@ Can we do better?
 
 Yes we can but not by making comparisons.  
 
+RADIX SORT
+Not making comparisons
+
+Radix sort is a special sorting algorithm that works on lists of numbers
+It never makes comparisons between elements!
+It exploits the fact that information about the size of a number is encoded in the number of digits.
 
 */
+
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+console.log(getDigit(7323, 2));
