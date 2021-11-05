7ec51cf43fb2e3e2960f9813ccfdbc380450e0fa
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-11-05 16:56:11 -0400

    created a quick sort function

--- app.js
+++ app.js
@@ -1515,3 +1515,43 @@ function fib(n) {
   }
   return fibNums[n];
 }
+
+/* 
+Quick Sort
+-Like merge sort, exploits the fact that arrays of 0 or 1 elements are always sorted.
+-Works by selecting one element (called the "pivot") and finding the index where the pivot should end up in the sorted array. 
+-Once the pivot is positioned approprately, quick sort can be applied on either side of the pivot. 
+*/
+
+function pivot(arr, start = [0], end = arr.length - 1) {
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIdx = start;
+
+  for (var i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); // 2
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
